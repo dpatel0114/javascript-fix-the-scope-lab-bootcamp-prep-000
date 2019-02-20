ea545394915b455e23d13b43262b79bcbc8620e9
Dhara Patel <1991dharapatel@gmail.com>
2019-02-20 22:27:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+function myAnimal() {
+  var animal = 'dog'
+  return animal
+}
+
+function yourAnimal() {
+  var animal = 'cat'
+  return animal
+}
+
+function add2(n) {
+  return n + 2
+  const two = 2
+}
+
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
+
+var theFunk = funkyFunction
+var theFunk = funkyFunction()()
